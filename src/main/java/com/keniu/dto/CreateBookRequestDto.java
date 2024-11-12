53ca15671fb259ce5c52a30c08a591f105f040f6
Iurii Keniu <kenu21@gmail.com>
2024-11-12 09:10:19 +0100

    Validation (#4)
    VALIDATION: implemented global exception handler and added validation Size

--- src/main/java/com/keniu/dto/CreateBookRequestDto.java
+++ src/main/java/com/keniu/dto/CreateBookRequestDto.java
@@ -1,5 +1,8 @@
 package com.keniu.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -10,10 +13,18 @@ import lombok.Data;
  */
 @Data
 public class CreateBookRequestDto {
+    @NotBlank
+    @Size(max = 255)
     private String title;
+    @NotBlank
+    @Size(max = 255)
     private String author;
+    @NotBlank
+    @Size(max = 255)
     private String isbn;
+    @NotNull
     private BigDecimal price;
     private String description;
+    @Size(max = 255)
     private String coverImage;
 }
