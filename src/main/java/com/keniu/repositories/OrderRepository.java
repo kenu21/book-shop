4f12872a8905240ec6ba7801f3b5202d73572c15
iurii <kenu21@gmail.com>
2024-12-04 11:34:58 +0100

    ORDER: refactored

--- src/main/java/com/keniu/repositories/OrderRepository.java
+++ src/main/java/com/keniu/repositories/OrderRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
     @EntityGraph(attributePaths = "user")
     Page<Order> findByUserId(Long userId, Pageable pageable);
-
 }
