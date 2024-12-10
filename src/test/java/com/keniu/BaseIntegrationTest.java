32d81e1bbb262c0acc99b0e10b95a59512b7df25
iurii <kenu21@gmail.com>
2024-12-10 18:10:44 +0100

    TESTS: refactored

--- src/test/java/com/keniu/BaseIntegrationTest.java
+++ src/test/java/com/keniu/BaseIntegrationTest.java
@@ -6,23 +6,23 @@ import org.testcontainers.containers.MySQLContainer;
 
 public abstract class BaseIntegrationTest {
 
+    private static final MySQLContainer<?> mySqlContainer;
     private static final String DB_IMAGE = "mysql:8";
-    private static final MySQLContainer<?> MYSQL_CONTAINER;
 
     static {
-        MYSQL_CONTAINER = new MySQLContainer<>(DB_IMAGE);
-        MYSQL_CONTAINER.start();
+        mySqlContainer = new MySQLContainer<>(DB_IMAGE);
+        mySqlContainer.start();
 
-        System.setProperty("TEST_DB_URL", MYSQL_CONTAINER.getJdbcUrl());
-        System.setProperty("TEST_DB_USERNAME", MYSQL_CONTAINER.getUsername());
-        System.setProperty("TEST_DB_PASSWORD", MYSQL_CONTAINER.getPassword());
+        System.setProperty("TEST_DB_URL", mySqlContainer.getJdbcUrl());
+        System.setProperty("TEST_DB_USERNAME", mySqlContainer.getUsername());
+        System.setProperty("TEST_DB_PASSWORD", mySqlContainer.getPassword());
     }
 
     @DynamicPropertySource
     static void registerDatabaseProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.url", mySqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySqlContainer::getUsername);
+        registry.add("spring.datasource.password", mySqlContainer::getPassword);
         registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
     }
 }
