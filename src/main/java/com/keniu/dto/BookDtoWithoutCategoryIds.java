a3981e4672146c367eee00661435c01075915747
Iurii Keniu <kenu21@gmail.com>
2024-12-11 09:27:35 +0100

    Tests (#13)
    TESTS: added tests to project

--- src/main/java/com/keniu/dto/BookDtoWithoutCategoryIds.java
+++ src/main/java/com/keniu/dto/BookDtoWithoutCategoryIds.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 public class BookDtoWithoutCategoryIds {
     private Long id;
+    private String title;
     private String author;
     private String isbn;
     private BigDecimal price;
---
+++ src/test/resources/add-book.sql
@@ -0,0 +1,8 @@
+INSERT INTO books (id, title, author, isbn, price, description, cover_image, is_deleted)
+VALUES (1, 'Test Book', 'Author1', '123-4567890123', 10.99, 'Description1', 'cover1.jpg', 0);
+
+INSERT INTO categories (id, name, description, is_deleted)
+VALUES (1, 'Fiction', 'Fictional books', 0);
+
+INSERT INTO books_categories (book_id, category_id)
+VALUES (1, 1);
